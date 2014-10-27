247515bb9a31e6ea6e96791b46e2aaffea44c368
KillianSaintcricq <killian.40@hotmail.fr>
2014-10-27 14:06:30 +0100

    Résolution des merges

---
+++ src/main/java/emn/tp/servlet/Register.java
@@ -0,0 +1,90 @@
+package emn.tp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import emn.tp.bean.jpa.UsersEntity;
+import emn.tp.persistence.PersistenceServiceProvider;
+import emn.tp.persistence.services.UsersPersistence;
+import emn.tp.services.implementation.RegisterService;
+import emn.tp.services.interfaces.RegisterServiceInterface;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet({"/Register"})
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("REGISTER : Forwarding to Register JSP...");
+		request.getRequestDispatcher("/WEB-INF/jsp/Register.jsp").forward(request, response);
+		System.out.println("REGISTER : Removing registerError session variable...");
+		request.getSession().removeAttribute("registerError");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Register service (database work)
+		RegisterServiceInterface regService = new RegisterService();
+
+		// Get inputs
+		System.out.println("REGISTER : Getting inputs...");
+		String pseudo = request.getParameter("pseudo") ;
+		String mail = request.getParameter("mail") ;
+		String password = request.getParameter("password") ;
+		String passwordConfirmation = request.getParameter("passwordConfirmation") ;
+		
+		// If they are not all filled
+		System.out.println("REGISTER : Validating fields...");
+		if (!regService.validateField(pseudo, mail, password, passwordConfirmation)){
+			System.out.println("REGISTER : Inputs not filled...");
+			request.getSession().setAttribute("registerError", "Tous les champs doivent être renseignés.");
+			response.sendRedirect("Register");
+		// If the two passwords are not the same
+		}else if (!regService.validatePassword(request.getParameter("password"), request.getParameter("passwordConfirmation"))) {
+			System.out.println("REGISTER : The two passwords are not the same...");
+			request.getSession().setAttribute("registerError", "Les deux mots de passe doivent correspondre.");
+			response.sendRedirect("Register");
+		// If mail already exists
+		}else if(!regService.checkMail(mail)){
+			System.out.println("REGISTER : Mail already exists...");
+			request.getSession().setAttribute("registerError", "Un compte existe déjà pour l'adresse mail renseignée.");
+			response.sendRedirect("Register");
+		// If inputs are correctly filled
+		}else{
+			// Insert 
+			System.out.println("REGISTER : Creating user entity...");
+			UsersPersistence serviceUsers = PersistenceServiceProvider.getService(UsersPersistence.class);
+			UsersEntity user = new UsersEntity();
+			user.setPseudo(pseudo);
+			user.setMail(mail);
+			user.setPassword(password);
+	    	System.out.println("REGISTER : Inserting into databse...");
+	    	serviceUsers.insert(user);
+			// Redirection
+			response.sendRedirect("Login");
+		}
+		
+	}
+
+}
